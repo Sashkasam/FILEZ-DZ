0e8f8dce74233cadd463dc123da87e8ed15b04c0
Aleksandr Samoilov <sparco@nxt.ru>
2022-09-21 13:51:33 +0500

    Second task done

--- main.py
+++ main.py
@@ -15,5 +15,23 @@ with open('book_of_recipes.txt', encoding='utf8') as reciepes:
             cook_book[dishes_name] = dishes
         reciepes.readline()
         
-    print(cook_book)
+    # print(cook_book)
     
+  # Задание № 2   написать функцию, которая на вход принимает список блюд из cook_book 
+  # и количество персон для кого мы будем готовить
+
+def shop_list_by_dishes():
+    shop_list_dishes = {}
+    print("Я помогу посчитать сколько нужно купить продуктов:)")
+    dishes = input("Введите название блюда(блюд) через запятую: ").split(', ')
+    person_count =  int(input("Введите количество человек: "))
+    for dish in dishes:
+        for ingridient in cook_book[dish]:
+            new_dict = dict(ingridient)
+            new_dict.pop('ingridient_name')
+            new_dict['quanity'] = int(new_dict['quanity']) * person_count
+            shop_list_dishes = {ingridient['ingridient_name'] : new_dict}
+            print(shop_list_dishes)
+
+
+shop_list_by_dishes()

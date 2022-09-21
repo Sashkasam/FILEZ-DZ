60cd920a69505817c154bc57c47103262f325460
Aleksandr Samoilov <sparco@nxt.ru>
2022-09-21 17:32:38 +0500

    Finish  dz

--- main.py
+++ main.py
@@ -34,4 +34,72 @@ def shop_list_by_dishes():
             print(shop_list_dishes)
 
 
-shop_list_by_dishes()
+# shop_list_by_dishes()
+
+
+#Задание № 3 
+with open('1.txt', encoding='utf8') as text1:
+    text1_read = text1.read() 
+    text1_name = '1.txt' 
+    text1.seek(0)
+    text1_full = text1.readlines()
+    text1_len = str(len(text1_full))
+
+with open('2.txt', encoding='utf8') as text2:
+    text2_read = text2.read()
+    text2_name = '2.txt'
+    text2.seek(0)
+    text2_full = text2.readlines()
+    text2_len = str(len(text2_full))
+    
+
+
+with open('3.txt', encoding='utf8') as text3:
+    text3_read = text3.read()
+    text3_name = '3.txt'
+    text3.seek(0)
+    text3_full = text3.readlines()
+    text3_len = str(len(text3_full))
+   
+   
+with open('result.txt','w', encoding='utf8') as result_text:
+    if min(text1_len,text2_len,text3_len) == text1_len:
+        result_text.write(text1_name + '\n'  + text1_len + '\n')
+        result_text.write(text1_read + '\n')
+        if min(text2_len,text3_len) == text2_len:
+            result_text.write(text2_name + '\n'  + text2_len + '\n')
+            result_text.write(text2_read + '\n')
+            result_text.write(text3_name + '\n'  + text3_len + '\n')
+            result_text.write(text3_read + '\n')
+        else:
+            result_text.write(text3_name + '\n'  + text3_len + '\n')
+            result_text.write(text3_read + '\n')
+            result_text.write(text2_name + '\n'  + text2_len + '\n')
+            result_text.write(text2_read + '\n')
+    elif  min(text1_len,text2_len,text3_len) == text2_len: 
+        result_text.write(text2_name + '\n'  + text2_len + '\n')
+        result_text.write(text2_read + '\n')
+        if min(text1_len,text3_len) == text1_len:
+            result_text.write(text1_name + '\n'  + text1_len + '\n')
+            result_text.write(text1_read + '\n')
+            result_text.write(text3_name + '\n'  + text3_len + '\n')
+            result_text.write(text3_read + '\n')
+        else:
+            result_text.write(text3_name + '\n'  + text3_len + '\n')
+            result_text.write(text3_read + '\n')
+            result_text.write(text1_name + '\n'  + text1_len + '\n')
+            result_text.write(text1_read + '\n')
+    else:
+        result_text.write(text3_name + '\n'  + text3_len + '\n')
+        result_text.write(text3_read + '\n')
+        if min(text1_len,text2_len) == text1_len:
+            result_text.write(text1_name + '\n'  + text1_len + '\n')
+            result_text.write(text1_read + '\n')
+            result_text.write(text2_name + '\n'  + text2_len + '\n')
+            result_text.write(text2_read + '\n')
+        else:
+            result_text.write(text2_name + '\n'  + text2_len + '\n')
+            result_text.write(text2_read + '\n')
+            result_text.write(text1_name + '\n'  + text1_len + '\n')
+            result_text.write(text1_read + '\n')
+       
